16d1b147919b57edb8d07ac1141f66a6519dc2b9
ZenithClown <debmalyapramanik.005@gmail.com>
2025-07-14 13:20:18 +0530

    ✨ added SourceControl() data class with default attributes - the data class should be used to control remote sources parameter with an option to override values - the same parameter name should be made available from cli for control

--- polyskills/remote/sources.py
+++ polyskills/remote/sources.py
@@ -20,6 +20,27 @@ fetch data using :mod:`requests` is supported.
 """
 
 from enum import Enum
+from dataclasses import dataclass
 
 class ValidSources(Enum):
     GITHUB = "https://www.github.com/"
+
+
+@dataclass(frozen = True)
+class SourceControl:
+    """
+    Additional parameters for remote source data control. These are
+    typically for advanced users.
+
+    :type  pagination: int
+    :param pagination: A pagination parameter controls how manye
+        results are returned in one page for a ``GET`` request from
+        ``https://api.github.com/repos/{owner}/{repo}/tags/`` (same
+        can be applicable for other websites). Defaults to 100, which
+        is the maximum value allowed.
+
+    Each parameter should be available as a CLI command for dynamic
+    control which sets the data class defaults during runtime.
+    """
+
+    pagination : int = 100
